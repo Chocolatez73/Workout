ece9135e1bcf7814840a45ee6a1d09c58a90a8e2
Chocolatez73 <xboxchcolatez73@gmail.com>
2020-11-16 19:19:30 +0800

    first gui, events not yet applied

---
+++ src/index.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Nano
+ */
+public class index {
+    public static void main(String[] args) {
+        MainPanel exe = new MainPanel();
+        exe.setVisible(true);
+    }
+}
